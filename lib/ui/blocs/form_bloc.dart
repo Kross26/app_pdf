c1e2973c56e55c7a7bd829220c0e05948e43f880
Diego Rios <riosdiego498@gmail.com>
2024-03-29 20:35:27 -0300

    first commit

---
+++ lib/ui/blocs/form_bloc.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FormBloc extends StatefulWidget {
+  const FormBloc({super.key});
+
+  @override
+  State<FormBloc> createState() => _FormBlocState();
+}
+
+class _FormBlocState extends State<FormBloc> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/ui/blocs/form_screen_bloc.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FormScreenBloc extends StatefulWidget {
+  const FormScreenBloc({super.key});
+
+  @override
+  State<FormScreenBloc> createState() => _FormScreenBlocState();
+}
+
+class _FormScreenBlocState extends State<FormScreenBloc> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
