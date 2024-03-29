ce790695e2d6cd4016d4be1ee0e07a984ff3a02c
Diego Rios <riosdiego498@gmail.com>
2024-03-29 20:36:19 -0300

    created views

---
+++ lib/ui/pages/views/archives_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ArchivesView extends StatelessWidget {
+  const ArchivesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
