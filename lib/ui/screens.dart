21a91749b35d72b62074cfd3db33d7998e3e96f6
Diego Rios <riosdiego498@gmail.com>
2024-03-29 20:35:15 -0300

    more routes added

--- lib/ui/screens.dart
+++ lib/ui/screens.dart
@@ -1,2 +1,5 @@
+export 'package:app_pdf/ui/pages/views/archives_view.dart';
+export 'package:app_pdf/ui/pages/views/configuration_view.dart';
+export 'package:app_pdf/ui/pages/screens/home_screen.dart';
+export 'package:app_pdf/ui/pages/views/home_view.dart';
 export 'package:app_pdf/ui/widgets/floating_button.dart';
-export 'package:app_pdf/ui/pages/home_screen.dart';
