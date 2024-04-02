91859e6121417be51bb5482d617b7026e13c0335
Diego Rios <riosdiego498@gmail.com>
2024-04-02 17:05:03 -0300

    background color added

--- lib/ui/pages/views/configuration_view.dart
+++ lib/ui/pages/views/configuration_view.dart
@@ -5,6 +5,8 @@ class ConfigurationView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      backgroundColor: Color.fromRGBO(211, 211, 211, 1),
+    );
   }
 }
