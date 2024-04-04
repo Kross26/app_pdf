b12d9b933e4a4c966a444edeb7449790932e95be
Diego <riosdiego498@gmail.com>
2024-04-04 16:35:26 -0300

    widget added

--- lib/ui/widgets/row_item_cuenta.dart
+++ lib/ui/widgets/row_item_cuenta.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
 class RowItemCuenta extends StatelessWidget {
-  const RowItemCuenta({super.key});
+  final IconData icon;
+  const RowItemCuenta({
+    super.key,
+    required this.icon,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double screenWidht = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Padding(
+      padding: const EdgeInsets.all(3.0),
+      child: SizedBox(
+        height: screenHeight * 0.2,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Icon(icon),
+              SizedBox(width: screenWidht * 0.03),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
