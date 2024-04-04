af627e05137a378a0d07e81c0cfb84550ba14ae8
Diego <riosdiego498@gmail.com>
2024-04-04 16:35:13 -0300

    new functions added

--- lib/ui/pages/views/home_view.dart
+++ lib/ui/pages/views/home_view.dart
@@ -9,220 +9,222 @@ class HomeView extends StatelessWidget {
     double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: const Color.fromRGBO(211, 211, 211, 1),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SizedBox(
-          // height: screenHeight * 0.17,
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  SizedBox(
-                    width: screenWidht * 0.10,
-                    height: screenHeight * 0.02,
-                    child: Text(
-                      'CREAR',
-                      style: TextStyle(
-                        fontSize: screenWidht * 0.04,
-                        fontWeight: FontWeight.w300,
-                        fontFamily: 'BebasNeue',
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Expanded(
-                    child: Card(
-                      margin: const EdgeInsets.all(10),
-                      elevation: 10,
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.grey[200]),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Image.asset(
-                                'assets/crear.png',
-                                width: screenWidht * 0.15,
-                              ),
-                            ),
-                            Text(
-                              'Crear pdf',
-                              style: TextStyle(fontSize: screenWidht * 0.03),
-                            ),
-                          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            // height: screenHeight * 0.17,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    SizedBox(
+                      width: screenWidht * 0.10,
+                      height: screenHeight * 0.03,
+                      child: Text(
+                        'CREAR',
+                        style: TextStyle(
+                          fontSize: screenWidht * 0.04,
+                          fontWeight: FontWeight.w300,
+                          fontFamily: 'BebasNeue',
                         ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: Card(
-                      margin: const EdgeInsets.all(10),
-                      elevation: 10,
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.grey[200]),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Image.asset(
-                                'assets/crear.png',
-                                width: screenWidht * 0.15,
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Expanded(
+                      child: Card(
+                        margin: const EdgeInsets.all(10),
+                        elevation: 10,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.grey[200]),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Image.asset(
+                                  'assets/crear.png',
+                                  width: screenWidht * 0.15,
+                                ),
                               ),
-                            ),
-                            Text(
-                              'Crear pdf',
-                              style: TextStyle(fontSize: screenWidht * 0.03),
-                            ),
-                          ],
+                              Text(
+                                'Crear pdf',
+                                style: TextStyle(fontSize: screenWidht * 0.03),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              //
-              //
-              //
-              //
-              Row(
-                children: [
-                  SizedBox(
-                    width: screenWidht * 0.15,
-                    height: screenHeight * 0.02,
-                    child: Text(
-                      'CONVERTIR',
-                      style: TextStyle(
-                        fontSize: screenWidht * 0.04,
-                        fontWeight: FontWeight.w300,
-                        fontFamily: 'BebasNeue',
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Expanded(
-                    child: Card(
-                      margin: const EdgeInsets.all(10),
-                      elevation: 10,
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.grey[200]),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Image.asset(
-                                'assets/crear.png',
-                                width: screenWidht * 0.15,
+                    Expanded(
+                      child: Card(
+                        margin: const EdgeInsets.all(10),
+                        elevation: 10,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.grey[200]),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Image.asset(
+                                  'assets/crear.png',
+                                  width: screenWidht * 0.15,
+                                ),
                               ),
-                            ),
-                            Text(
-                              'Crear pdf',
-                              style: TextStyle(fontSize: screenWidht * 0.03),
-                            ),
-                          ],
+                              Text(
+                                'Crear pdf',
+                                style: TextStyle(fontSize: screenWidht * 0.03),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: Card(
-                      margin: const EdgeInsets.all(10),
-                      elevation: 10,
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.grey[200]),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Image.asset(
-                                'assets/crear.png',
-                                width: screenWidht * 0.15,
-                              ),
-                            ),
-                            Text(
-                              'Crear pdf',
-                              style: TextStyle(fontSize: screenWidht * 0.03),
-                            ),
-                          ],
+                  ],
+                ),
+                //
+                //
+                //
+                //
+                Row(
+                  children: [
+                    SizedBox(
+                      width: screenWidht * 0.15,
+                      height: screenHeight * 0.03,
+                      child: Text(
+                        'CONVERTIR',
+                        style: TextStyle(
+                          fontSize: screenWidht * 0.04,
+                          fontWeight: FontWeight.w300,
+                          fontFamily: 'BebasNeue',
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Expanded(
-                    child: Card(
-                      margin: const EdgeInsets.all(10),
-                      elevation: 10,
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.grey[200]),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Image.asset(
-                                'assets/crear.png',
-                                width: screenWidht * 0.15,
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Expanded(
+                      child: Card(
+                        margin: const EdgeInsets.all(10),
+                        elevation: 10,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.grey[200]),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Image.asset(
+                                  'assets/crear.png',
+                                  width: screenWidht * 0.15,
+                                ),
                               ),
-                            ),
-                            Text(
-                              'Crear pdf',
-                              style: TextStyle(fontSize: screenWidht * 0.03),
-                            ),
-                          ],
+                              Text(
+                                'Crear pdf',
+                                style: TextStyle(fontSize: screenWidht * 0.03),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: Card(
-                      margin: const EdgeInsets.all(10),
-                      elevation: 10,
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.grey[200]),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Image.asset(
-                                'assets/crear.png',
-                                width: screenWidht * 0.15,
+                    Expanded(
+                      child: Card(
+                        margin: const EdgeInsets.all(10),
+                        elevation: 10,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.grey[200]),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Image.asset(
+                                  'assets/crear.png',
+                                  width: screenWidht * 0.15,
+                                ),
                               ),
-                            ),
-                            Text(
-                              'Crear pdf',
-                              style: TextStyle(fontSize: screenWidht * 0.03),
-                            ),
-                          ],
+                              Text(
+                                'Crear pdf',
+                                style: TextStyle(fontSize: screenWidht * 0.03),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Expanded(
+                      child: Card(
+                        margin: const EdgeInsets.all(10),
+                        elevation: 10,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.grey[200]),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Image.asset(
+                                  'assets/crear.png',
+                                  width: screenWidht * 0.15,
+                                ),
+                              ),
+                              Text(
+                                'Crear pdf',
+                                style: TextStyle(fontSize: screenWidht * 0.03),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Card(
+                        margin: const EdgeInsets.all(10),
+                        elevation: 10,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.grey[200]),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Image.asset(
+                                  'assets/crear.png',
+                                  width: screenWidht * 0.15,
+                                ),
+                              ),
+                              Text(
+                                'Crear pdf',
+                                style: TextStyle(fontSize: screenWidht * 0.03),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
