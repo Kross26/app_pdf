143d2109bfe3e35bfc05f36bcc31b06b1aac7bd4
Diego <riosdiego498@gmail.com>
2024-04-04 16:35:53 -0300

    new functions added

--- lib/ui/widgets/row_item_premium.dart
+++ lib/ui/widgets/row_item_premium.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 
-class RowItem extends StatelessWidget {
+class RowItemPremium extends StatelessWidget {
   final IconData icon;
   final String title;
   final IconData icon2;
-  final String subtitle;
 
-  const RowItem({
+  const RowItemPremium({
     super.key,
     required this.icon,
     required this.title,
     required this.icon2,
-    required this.subtitle,
   });
 
   @override
@@ -19,28 +17,39 @@ class RowItem extends StatelessWidget {
     double screenWidht = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
+      padding: const EdgeInsets.all(3.0),
+      child: SizedBox(
         height: screenHeight * 0.6,
-        child: Row(
-          children: [
-            Icon(icon),
-            SizedBox(width: screenWidht * 0.08),
-            Text(
-              title,
-              style: TextStyle(fontSize: screenHeight * 0.019),
-            ),
-            SizedBox(width: screenWidht * 0.4),
-            Icon(icon2),
-            Column(
-              children: [
-                Text(
-                  subtitle,
-                  style: TextStyle(fontSize: screenHeight * 0.014),
-                ),
-              ],
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Icon(icon),
+              SizedBox(width: screenWidht * 0.06),
+              Column(
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: screenHeight * 0.019,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                  const Column(
+                    children: [
+                      Text(
+                        'Acceso a premium',
+                        textAlign: TextAlign.left,
+                      )
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(width: screenWidht * 0.3),
+              Icon(icon2),
+            ],
+          ),
         ),
       ),
     );
