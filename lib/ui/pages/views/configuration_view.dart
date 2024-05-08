2501271baf2aafc058f726fa2631d2f9995d4885
Diego Rios <riosdiego498@gmail.com>
2024-05-08 19:20:41 -0400

    news implementations

--- lib/ui/pages/views/configuration_view.dart
+++ lib/ui/pages/views/configuration_view.dart
@@ -1,10 +1,16 @@
 import 'package:app_pdf/ui/widgets/row_item_cuenta.dart';
 import 'package:flutter/material.dart';
-
 import '../../screens.dart';
 
-class ConfigurationView extends StatelessWidget {
+class ConfigurationView extends StatefulWidget {
   const ConfigurationView({super.key});
+
+  @override
+  State<ConfigurationView> createState() => _ConfigurationViewState();
+}
+
+class _ConfigurationViewState extends State<ConfigurationView> {
+  bool _isSelected = false;
 
   @override
   Widget build(BuildContext context) {
@@ -12,284 +18,28 @@ class ConfigurationView extends StatelessWidget {
     double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: const Color.fromRGBO(211, 211, 211, 1),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Row(
-                children: [
-                  SizedBox(
-                    width: screenWidht * 0.14,
-                    height: screenHeight * 0.03,
-                    child: Text(
-                      'PREMIUM',
-                      style: TextStyle(
-                        fontSize: screenWidht * 0.04,
-                        fontWeight: FontWeight.w300,
-                        fontFamily: 'BebasNeue',
-                      ),
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(height: screenHeight * 0.01),
-              Column(
-                children: [
-                  Container(
-                    height: screenHeight * 0.10,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: const Color.fromRGBO(255, 255, 255, 1)),
-                    child: const RowItemPremium(
-                      icon: Icons.star,
-                      title: 'Obtener Premium',
-                      icon2: Icons.arrow_forward,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: screenHeight * 0.007),
-              Row(
-                children: [
-                  SizedBox(
-                    width: screenWidht * 0.15,
-                    height: screenHeight * 0.03,
-                    child: Text(
-                      'CUENTA',
-                      style: TextStyle(
-                        fontSize: screenWidht * 0.04,
-                        fontWeight: FontWeight.w300,
-                        fontFamily: 'BebasNeue',
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: screenHeight * 0.01),
-              Container(
-                height: screenHeight * 0.07,
-                decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(10),
-                      topLeft: Radius.circular(10),
-                    ),
-                    color: Colors.white),
-                child: const Padding(
-                  padding: EdgeInsets.only(left: 10, right: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      RowItemCuenta(
-                        icon: Icons.person_2_outlined,
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            'Licencia',
-                            textAlign: TextAlign.left,
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                      // SizedBox(width: screenWidht * 0.4),
-                      Row(
-                        children: [
-                          Text('Gratuita'),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                height: screenHeight * 0.07,
-                decoration: const BoxDecoration(color: Colors.white),
-                child: const Padding(
-                  padding: EdgeInsets.only(left: 10, right: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      RowItemCuenta(
-                        icon: Icons.person_2_outlined,
-                      ),
-                      Text(
-                        'Documentos Restantes',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Row(
-                        children: [Text('0 de 3')],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                height: screenHeight * 0.07,
-                decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10)),
-                    color: Colors.white),
-                child: const Padding(
-                  padding: EdgeInsets.only(left: 10, right: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      RowItemCuenta(
-                        icon: Icons.person_2_outlined,
-                      ),
-                      Text(
-                        'Bloqueo Huella',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      // SizedBox(width: screenWidht * 0.26),
-                      Row(
-                        children: [Text('Premium')],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(height: screenHeight * 0.007),
-              Row(
-                children: [
-                  SizedBox(
-                    width: screenWidht * 0.15,
-                    height: screenHeight * 0.03,
-                    child: Text(
-                      'GENERAL',
-                      style: TextStyle(
-                        fontSize: screenWidht * 0.04,
-                        fontWeight: FontWeight.w300,
-                        fontFamily: 'BebasNeue',
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: screenHeight * 0.01),
-              Container(
-                height: screenHeight * 0.07,
-                decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(10),
-                      topLeft: Radius.circular(10),
-                    ),
-                    color: Colors.white),
-                child: const Padding(
-                  padding: EdgeInsets.only(left: 10, right: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      RowItemCuenta(
-                        icon: Icons.sync,
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            'Suscripción',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                      // SizedBox(width: screenWidht * 0.424),
-                      Row(
-                        children: [
-                          Icon(Icons.arrow_forward),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                height: screenHeight * 0.07,
-                decoration: const BoxDecoration(color: Colors.white),
-                child: const Padding(
-                  padding: EdgeInsets.only(left: 10, right: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      RowItemCuenta(
-                        icon: Icons.person_2_outlined,
-                      ),
-                      Text(
-                        'Administar Suscripciones',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      // SizedBox(width: screenWidht * 0.17),
-                      Row(
-                        children: [
-                          Icon(Icons.arrow_forward),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                height: screenHeight * 0.07,
-                decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(0),
-                        bottomRight: Radius.circular(0)),
-                    color: Colors.white),
-                child: const Padding(
-                  padding: EdgeInsets.only(right: 20, left: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      RowItemCuenta(
-                        icon: Icons.folder_open_outlined,
-                      ),
-                      Text(
-                        'Preguntas más frecuentes',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      // SizedBox(width: screenWidht * 0.16),
-                      Row(
-                        children: [
-                          Icon(Icons.arrow_forward),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                height: screenHeight * 0.07,
-                decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10)),
-                    color: Colors.white),
-                child: const Padding(
-                  padding: EdgeInsets.only(right: 20, left: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      RowItemCuenta(
-                        icon: Icons.chat_bubble_outline,
-                      ),
-                      Text(
-                        'Atención al cliente',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      // SizedBox(width: screenWidht * 0.16),
-                      Row(
-                        children: [
-                          Icon(Icons.arrow_forward),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
+      body: Column(
+        children: [
+          Text('Premium', style: TextStyle()),
+          RowItemPremium(),
+          Text('Cuenta'),
+          RowItemCuenta(
+            iconCuenta: Icon(Icons.account_circle_outlined),
+            titleCuenta: Text('Licencia'),
+            trailing: Text('Gratuita'),
+            trailingSwitch: null,
           ),
-        ),
+          RowItemCuenta(
+            iconCuenta: Icon(Icons.account_circle_outlined),
+            titleCuenta: Text('Documentos Restantes'),
+            trailing: Text('0 de 3'),
+            trailingSwitch: null,
+          ),
+          RowItemCuenta(
+            iconCuenta: Icon(Icons.account_circle_outlined),
+            titleCuenta: Text('Desbloqueo con Huella'),
+          ),
+        ],
       ),
     );
   }
