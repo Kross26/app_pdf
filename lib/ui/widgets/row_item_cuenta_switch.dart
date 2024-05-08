46b24b91e85b0295355a795feffba645d347d00c
Diego Rios <riosdiego498@gmail.com>
2024-05-08 19:21:43 -0400

    new widget

---
+++ lib/ui/widgets/row_item_cuenta_switch.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class RowItemCuentaSwitch extends StatefulWidget {
+  final Switch? trailingSwitch;
+  const RowItemCuentaSwitch({
+    super.key,
+    this.trailingSwitch,
+  });
+
+  @override
+  State<RowItemCuentaSwitch> createState() => _RowItemCuentaSwitchState();
+}
+
+class _RowItemCuentaSwitchState extends State<RowItemCuentaSwitch> {
+  bool _isSelected = false;
+  @override
+  Widget build(BuildContext context) {
+    // double screenWidht = MediaQuery.of(context).size.width;
+    // double screenHeight = MediaQuery.of(context).size.height;
+    return Switch(
+      value: _isSelected,
+      onChanged: (newValue) {},
+    );
+  }
+}
