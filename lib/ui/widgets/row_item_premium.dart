7bb2094c758fd696bfc866b10bc9c5d1fb3f9bb2
Diego Rios <riosdiego498@gmail.com>
2024-05-08 19:21:37 -0400

    new widget

--- lib/ui/widgets/row_item_premium.dart
+++ lib/ui/widgets/row_item_premium.dart
@@ -1,61 +1,26 @@
 import 'package:flutter/material.dart';
 
 class RowItemPremium extends StatelessWidget {
-  final IconData icon;
-  final String title;
-  final IconData icon2;
-
   const RowItemPremium({
     super.key,
-    required this.icon,
-    required this.title,
-    required this.icon2,
   });
 
   @override
   Widget build(BuildContext context) {
-    double screenWidht = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
+    // double screenWidht = MediaQuery.of(context).size.width;
+    // double screenHeight = MediaQuery.of(context).size.height;
     return Padding(
-      padding: const EdgeInsets.all(3.0),
-      child: SizedBox(
-        height: screenHeight * 0.6,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Icon(icon),
-              // SizedBox(width: screenWidht * 0.06),
-              Padding(
-                padding: const EdgeInsets.only(top: 3),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  // mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      title,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: screenHeight * 0.019,
-                      ),
-                      textAlign: TextAlign.left,
-                    ),
-                    const Column(
-                      children: [
-                        Text(
-                          'Acceso a premium',
-                          textAlign: TextAlign.left,
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(width: screenWidht * 0.3),
-              Icon(icon2),
-            ],
-          ),
+      padding: EdgeInsets.all(8.0),
+      child: Card(
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+        child: ListTile(
+          leading: Icon(Icons.star_border_outlined),
+          title: Text('Obtener Premium'),
+          subtitle: Text('Acceso a premium'),
+          onTap: () {},
+          trailing: Icon(Icons.arrow_forward_ios_outlined),
+          tileColor: Colors.white,
         ),
       ),
     );
