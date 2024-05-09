148e23ac47d10442fab7c7396c44c0412b22a6c2
Diego <riosdiego498@gmail.com>
2024-05-09 15:00:03 -0400

    minor fixes

--- lib/ui/pages/views/configuration_view.dart
+++ lib/ui/pages/views/configuration_view.dart
@@ -1,4 +1,5 @@
 import 'package:app_pdf/ui/widgets/row_item_cuenta.dart';
+import 'package:app_pdf/ui/widgets/row_item_general.dart';
 import 'package:flutter/material.dart';
 import '../../screens.dart';
 
@@ -10,36 +11,106 @@ class ConfigurationView extends StatefulWidget {
 }
 
 class _ConfigurationViewState extends State<ConfigurationView> {
-  bool _isSelected = false;
+  // bool _isSelected = false;
 
   @override
   Widget build(BuildContext context) {
-    double screenWidht = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
+    // double screenWidht = MediaQuery.of(context).size.width;
+    // double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: const Color.fromRGBO(211, 211, 211, 1),
-      body: Column(
-        children: [
-          Text('Premium', style: TextStyle()),
-          RowItemPremium(),
-          Text('Cuenta'),
-          RowItemCuenta(
-            iconCuenta: Icon(Icons.account_circle_outlined),
-            titleCuenta: Text('Licencia'),
-            trailing: Text('Gratuita'),
-            trailingSwitch: null,
-          ),
-          RowItemCuenta(
-            iconCuenta: Icon(Icons.account_circle_outlined),
-            titleCuenta: Text('Documentos Restantes'),
-            trailing: Text('0 de 3'),
-            trailingSwitch: null,
-          ),
-          RowItemCuenta(
-            iconCuenta: Icon(Icons.account_circle_outlined),
-            titleCuenta: Text('Desbloqueo con Huella'),
-          ),
-        ],
+      body: SafeArea(
+        bottom: false,
+        child: ListView(
+          children: const [
+            SizedBox(height: 5),
+            Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text('Premium'),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: RowItemPremium(),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text('Cuenta'),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: RowItemCuenta(
+                iconCuenta: Icon(Icons.account_circle_outlined),
+                titleCuenta: Text('Licencia'),
+                trailing: Text('Gratuita'),
+                trailingSwitch: null,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: RowItemCuenta(
+                iconCuenta: Icon(Icons.account_circle_outlined),
+                titleCuenta: Text('Documentos Restantes'),
+                trailing: Text('0 de 3'),
+                trailingSwitch: null,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: RowItemCuenta(
+                iconCuenta: Icon(Icons.account_circle_outlined),
+                titleCuenta: Text('Desbloqueo con Huella'),
+                trailing: Text('Premium'),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text('General'),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: RowItemGeneral(
+                iconCuenta: Icon(Icons.refresh_outlined),
+                titleCuenta: Text('Suscripcíon'),
+                trailing: Icon(Icons.arrow_forward_outlined),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: RowItemGeneral(
+                iconCuenta: Icon(Icons.attach_money_outlined),
+                titleCuenta: Text('Administrar Suscripciones'),
+                trailing: Icon(Icons.arrow_forward_outlined),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: RowItemGeneral(
+                iconCuenta: Icon(Icons.question_mark_outlined),
+                titleCuenta: Text('Preguntas más frecuentes'),
+                trailing: Icon(Icons.arrow_forward_outlined),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: RowItemGeneral(
+                iconCuenta: Icon(Icons.chat_bubble_outline_outlined),
+                titleCuenta: Text('Contactar al Soporte'),
+                trailing: Icon(Icons.arrow_forward_outlined),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: RowItemGeneral(
+                iconCuenta: Icon(Icons.info_outline),
+                titleCuenta: Text('Informar de un error'),
+                trailing: Icon(Icons.arrow_forward_outlined),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            )
+          ],
+        ),
       ),
     );
   }
