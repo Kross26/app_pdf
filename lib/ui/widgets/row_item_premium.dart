2ddf1f411e76c616dc92be624c1c2456bcadcd40
Diego <riosdiego498@gmail.com>
2024-05-09 15:01:00 -0400

    padding remove

--- lib/ui/widgets/row_item_premium.dart
+++ lib/ui/widgets/row_item_premium.dart
@@ -9,19 +9,15 @@ class RowItemPremium extends StatelessWidget {
   Widget build(BuildContext context) {
     // double screenWidht = MediaQuery.of(context).size.width;
     // double screenHeight = MediaQuery.of(context).size.height;
-    return Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Card(
-        shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
-        child: ListTile(
-          leading: Icon(Icons.star_border_outlined),
-          title: Text('Obtener Premium'),
-          subtitle: Text('Acceso a premium'),
-          onTap: () {},
-          trailing: Icon(Icons.arrow_forward_ios_outlined),
-          tileColor: Colors.white,
-        ),
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+      child: ListTile(
+        leading: const Icon(Icons.star_border_outlined),
+        title: const Text('Obtener Premium'),
+        subtitle: const Text('Acceso a premium'),
+        onTap: () {},
+        trailing: const Icon(Icons.arrow_forward_outlined),
+        tileColor: Colors.white,
       ),
     );
   }
