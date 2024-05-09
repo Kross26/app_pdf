6f50a24fe82ff20cc97f05b7e6f7de3e06a39e2d
Diego <riosdiego498@gmail.com>
2024-05-09 15:00:51 -0400

    minor fixes

--- lib/ui/widgets/row_item_general.dart
+++ lib/ui/widgets/row_item_general.dart
@@ -1,56 +1,28 @@
 import 'package:flutter/material.dart';
 
 class RowItemGeneral extends StatelessWidget {
-  final IconData icon;
-  final String title;
-  final IconData icon2;
-
+  final Icon iconCuenta;
+  final Text titleCuenta;
+  final Icon trailing;
   const RowItemGeneral({
     super.key,
-    required this.icon,
-    required this.title,
-    required this.icon2,
+    required this.iconCuenta,
+    required this.titleCuenta,
+    required this.trailing,
   });
 
   @override
   Widget build(BuildContext context) {
-    double screenWidht = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-    return Padding(
-      padding: const EdgeInsets.all(3.0),
-      child: SizedBox(
-        height: screenHeight * 0.6,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              Icon(icon),
-              SizedBox(width: screenWidht * 0.06),
-              Column(
-                children: [
-                  Text(
-                    title,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: screenHeight * 0.019,
-                    ),
-                    textAlign: TextAlign.left,
-                  ),
-                  const Column(
-                    children: [
-                      Text(
-                        'Acceso a premium',
-                        textAlign: TextAlign.left,
-                      )
-                    ],
-                  )
-                ],
-              ),
-              SizedBox(width: screenWidht * 0.3),
-              Icon(icon2),
-            ],
-          ),
-        ),
+    // double screenWidht = MediaQuery.of(context).size.width;
+    // double screenHeight = MediaQuery.of(context).size.height;
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+      child: ListTile(
+        leading: iconCuenta,
+        title: titleCuenta,
+        onTap: () {},
+        trailing: trailing,
+        tileColor: Colors.white,
       ),
     );
   }
