38502ce56df3baf12ecb4fd58aada4e2e4b55e04
Diego Rios <riosdiego498@gmail.com>
2024-05-15 12:14:56 -0400

    fixes

--- lib/ui/widgets/invoiceForm/invoice_form.dart
+++ lib/ui/widgets/invoiceForm/invoice_form.dart
@@ -1,108 +1,122 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:pdf/pdf.dart';
-import 'package:pdf/widgets.dart' as pw;
-import 'package:open_file/open_file.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Factura PDF',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Factura PDF'),
-        ),
-        body: InvoiceForm(),
-      ),
-    );
-  }
-}
+// se define un enumerador con 3 opciones
+enum ChangeItem { selected }
 
 class InvoiceForm extends StatefulWidget {
+  const InvoiceForm({super.key});
+
   @override
-  _InvoiceFormState createState() => _InvoiceFormState();
+  State<InvoiceForm> createState() => _InvoiceFormState();
 }
 
 class _InvoiceFormState extends State<InvoiceForm> {
-  File? _image;
-
-  Future<void> _getImage() async {
-    final picker = ImagePicker();
-    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
-    setState(() {
-      if (pickedFile != null) {
-        _image = File(pickedFile.path);
-      } else {
-        print('No image selected.');
-      }
-    });
-  }
-
-  Future<void> _generatePdf() async {
-    final pdf = pw.Document();
-
-    pdf.addPage(
-      pw.Page(
-        build: (pw.Context context) {
-          return pw.Center(
-            child: pw.Column(
-              children: [
-                pw.Text('Factura'),
-                pw.Text('Nombre del cliente: [obtener desde TextField]'),
-                pw.Text('Producto: [obtener desde TextField]'),
-                if (_image != null)
-                  pw.Image(
-                    pw.MemoryImage(
-                      _image!.readAsBytesSync(),
-                    ),
-                  ),
-              ],
-            ),
-          );
-        },
-      ),
-    );
-
-    final String dir = (await getApplicationDocumentsDirectory()).path;
-    final String path = '$dir/factura.pdf';
-    final File file = File(path);
-    await file.writeAsBytes(await pdf.save());
-
-    OpenFile.open(path); // Abre el PDF después de generarlo
-  }
+  ChangeItem? _item = ChangeItem.selected;
+  Color activeColor = const Color.fromRGBO(211, 211, 211, 1);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+    double screenWidht = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      // color del background
+      backgroundColor: const Color.fromRGBO(211, 211, 211, 1),
+      body: Column(
         children: [
-          TextField(
-            decoration: InputDecoration(labelText: 'Nombre del cliente'),
-          ),
-          SizedBox(height: 16.0),
-          TextField(
-            decoration: InputDecoration(labelText: 'Producto'),
-          ),
-          SizedBox(height: 16.0),
-          ElevatedButton(
-            onPressed: _getImage,
-            child: Text('Cargar imagen'),
-          ),
-          SizedBox(height: 16.0),
-          if (_image != null)
-            Image.file(
-              _image!,
-              height: 200,
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            // texto que se muestra al principio de la pantalla
+            child: Text(
+              'Selecciona una opción',
+              style: TextStyle(fontSize: screenWidht * 0.045),
             ),
-          SizedBox(height: 16.0),
+          ),
+          SizedBox(height: screenHeight * 0.025),
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 50, right: 50),
+                child: Container(
+                  // height: screenHeight * 0.05,
+                  // width: screenWidht * 0.8,
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(30))),
+                  child: RadioListTile(
+                    title: const Text('Patente'),
+                    value: ChangeItem.selected,
+                    groupValue: _item,
+                    onChanged: (ChangeItem? value) {
+                      setState(() {
+                        _item = value;
+                      });
+                    },
+                    // color al ser seleccionado
+                    activeColor: activeColor,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 5),
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 50, right: 50),
+                child: Container(
+                  // height: screenHeight * 0.05,
+                  // width: screenWidht * 0.8,
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(30))),
+                  child: RadioListTile(
+                    title: const Text('Marca'),
+                    value: ChangeItem.selected,
+                    groupValue: _item,
+                    onChanged: (ChangeItem? value) {
+                      setState(() {
+                        _item = value;
+                      });
+                    },
+                    activeColor: activeColor,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 5),
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 50, right: 50),
+                child: Container(
+                  // height: screenHeight * 0.05,
+                  // width: screenWidht * 0.8,
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(30))),
+                  child: RadioListTile(
+                    title: const Text('Chasis'),
+                    value: ChangeItem.selected,
+                    groupValue: _item,
+                    onChanged: (ChangeItem? value) {
+                      setState(() {
+                        _item = value;
+                      });
+                    },
+                    activeColor: activeColor,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: screenHeight * 0.015),
           ElevatedButton(
-            onPressed: _generatePdf,
-            child: Text('Generar PDF'),
+            onPressed: () {
+              // Imprimir los ítems seleccionados
+              // print('Ítems seleccionados: $selectedItems');
+            },
+            child: Text('Continuar'),
           ),
         ],
       ),
