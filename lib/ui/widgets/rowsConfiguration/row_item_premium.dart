2506e6aba84dbb5c650c14117dd486e7ea28f6ee
Diego <riosdiego498@gmail.com>
2024-05-16 17:17:53 -0400

    minor fixes

--- lib/ui/widgets/rowsConfiguration/row_item_premium.dart
+++ lib/ui/widgets/rowsConfiguration/row_item_premium.dart
@@ -10,14 +10,18 @@ class RowItemPremium extends StatelessWidget {
     // double screenWidht = MediaQuery.of(context).size.width;
     // double screenHeight = MediaQuery.of(context).size.height;
     return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
-      child: ListTile(
-        leading: const Icon(Icons.star_border_outlined),
-        title: const Text('Obtener Premium'),
-        subtitle: const Text('Acceso a premium'),
-        onTap: () {},
-        trailing: const Icon(Icons.arrow_forward_outlined),
-        tileColor: Colors.white,
+      // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+      child: Container(
+        decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        child: ListTile(
+          leading: const Icon(Icons.star_border_outlined),
+          title: const Text('Obtener Premium'),
+          subtitle: const Text('Acceso a premium'),
+          onTap: () {},
+          trailing: const Icon(Icons.arrow_forward_outlined),
+          tileColor: Colors.white,
+        ),
       ),
     );
   }
