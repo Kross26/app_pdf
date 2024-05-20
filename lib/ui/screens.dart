5ea7dd9b90a1385dd5004a02bbf1abb4f0d95de1
Diego <riosdiego498@gmail.com>
2024-05-20 17:20:32 -0400

    new screen path

--- lib/ui/screens.dart
+++ lib/ui/screens.dart
@@ -1,9 +1,11 @@
+export 'package:app_pdf/ui/pages/screens/imagepdf_screen.dart';
+
 export 'package:app_pdf/ui/widgets/textfield_screen.dart';
 
 export 'package:app_pdf/ui/pages/views/invoice_screen.dart';
 export 'package:app_pdf/ui/widgets/invoiceForm/invoice_form.dart';
 export 'package:app_pdf/ui/widgets/rowsConfiguration/row_item_cuenta_switch.dart';
-export 'package:app_pdf/ui/pages/views/document_viewer_screen.dart';
+export 'package:app_pdf/ui/pages/views/imagetopdf_view.dart';
 export 'package:app_pdf/ui/widgets/rowsConfiguration/row_item_premium.dart';
 export 'package:app_pdf/ui/pages/views/archives_view.dart';
 export 'package:app_pdf/ui/pages/views/configuration_view.dart';
